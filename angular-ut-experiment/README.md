1007b97ea7583b9befef42de1db63fa1842b75d4
loki2302 <loki2302@loki2302.me>
2015-01-08 17:11:54 +0300

    Added a unit-testing playground

---
+++ angular-ut-experiment/README.md
@@ -0,0 +1,13 @@
+Before running, install `karma-cli` globally:
+```
+npm install -g karma-cli
+```
+And sure do `npm install` and `bower install`. Then, to run tests one time:
+```
+npm run test-single-run
+```
+To continuously run tests:
+```
+npm test
+```
+
---
+++ angular-ut-experiment/karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+  config.set({
+    basePath: './',
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'hello.spec.js'
+    ],
+
+    autoWatch: true,
+    frameworks: ['jasmine'],
+    browsers: ['Chrome'],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ]
+  });
+};
