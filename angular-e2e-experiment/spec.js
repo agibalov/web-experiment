f973ac694ffbe82667bc40a0e03e844117eeaeb5
loki2302 <loki2302@loki2302.me>
2015-01-09 13:25:07 +0300

    Added a page-object variation of calculator tests

--- angular-e2e-experiment/spec.js
+++ angular-e2e-experiment/spec.js
@@ -14,22 +14,81 @@ describe('dummy nodejs app', function() {
     expect(element(by.id('asyncMessage')).getText()).toEqual('i am async message');
   });
 
-  it('has empty A, B and result', function() {
-    browser.get('/');
-    expect(element(by.model('a')).getText()).toEqual('');
-    expect(element(by.model('b')).getText()).toEqual('');
-    expect(element(by.binding('result')).getText()).toEqual('');
+  describe('calculator via locators', function() {
+    it('has empty A, B and result', function() {
+      browser.get('/');
+      expect(element(by.model('a')).getText()).toEqual('');
+      expect(element(by.model('b')).getText()).toEqual('');
+      expect(element(by.binding('result')).getText()).toEqual('');
+    });
+
+    it('gives me a sum of A and B when I type numbers and click submit', function() {
+      browser.get('/');
+
+      element(by.model('a')).sendKeys(2);
+      element(by.model('b')).sendKeys(3);
+      element(by.id('addNumbers')).click();
+      expect(element(by.binding('result')).getText()).toEqual('5');
+
+      expect(element(by.model('a')).getText()).toEqual('');
+      expect(element(by.model('b')).getText()).toEqual('');
+    });
   });
 
-  it('gives me a sum of A and B when I type numbers and click submit', function() {
-    browser.get('/');
+  describe('calculator via page object', function() {
+    it('has empty A, B and result', function() {
+      var page = new CalculatorPage();
+      page.get();
+      expect(page.getA()).toEqual('');
+      expect(page.getB()).toEqual('');
+      expect(page.getResult()).toEqual('');
+    });
 
-    element(by.model('a')).sendKeys(2);
-    element(by.model('b')).sendKeys(3);
-    element(by.id('addNumbers')).click();    
-    expect(element(by.binding('result')).getText()).toEqual('5');
+    it('gives me a sum of A and B when I type numbers and click submit', function() {
+      var page = new CalculatorPage();
+      page.get();
 
-    expect(element(by.model('a')).getText()).toEqual('');
-    expect(element(by.model('b')).getText()).toEqual('');
-  });
+      page.setA(2);
+      page.setB(3);
+      page.triggerAdd();
+      expect(page.getResult()).toEqual('5');
+      expect(page.getA()).toEqual('');
+      expect(page.getB()).toEqual('');
+    });
+
+    function CalculatorPage() {
+      this.aInput = element(by.model('a'));
+      this.bInput = element(by.model('b'));
+      this.result = element(by.binding('result'));
+      this.add = element(by.id('addNumbers'));
+
+      this.get = function() {
+        browser.get('/');
+      };
+
+      this.setA = function(a) {
+        this.aInput.sendKeys(a);
+      };
+
+      this.getA = function() {
+        return this.aInput.getText();
+      };
+
+      this.setB = function(b) {
+        this.bInput.sendKeys(b);
+      };
+
+      this.getB = function() {
+        return this.bInput.getText();
+      };
+
+      this.triggerAdd = function() {
+        this.add.click();
+      };
+
+      this.getResult = function() {
+        return this.result.getText();
+      };
+    };
+  });  
 });
