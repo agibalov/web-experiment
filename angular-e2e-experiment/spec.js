68781ba5d25535d18ea345a618c41f82741658c0
loki2302 <loki2302@loki2302.me>
2015-01-09 11:52:26 +0300

    Configured a base URL for tests

--- angular-e2e-experiment/spec.js
+++ angular-e2e-experiment/spec.js
@@ -1,16 +1,16 @@
 describe('dummy nodejs app', function() {
   it('should have page title set to Hello e2e', function() {
-    browser.get('http://localhost:1337/');
+    browser.get('/');
     expect(browser.getTitle()).toEqual('Hello e2e');
   });
 
   it('should have a welcome message', function() {
-    browser.get('http://localhost:1337/');
+    browser.get('/');
     expect(element(by.id('message')).getText()).toEqual('hi there!');
   });
 
   it('should have a second welcome message', function() {
-    browser.get('http://localhost:1337/');
+    browser.get('/');
     expect(element(by.id('asyncMessage')).getText()).toEqual('i am async message');
   });
 });
