7293e50b5bb370adf1fa429a95eeba056f96b51d
loki2302 <loki2302@loki2302.me>
2015-01-10 16:09:25 +0300

    Added one more test for dummy filter

--- angular-ut-experiment/filter.spec.js
+++ angular-ut-experiment/filter.spec.js
@@ -1,7 +1,11 @@
 angular.module('filterApp', [])
 .filter('reverse', function() {
   return function(text) {
-    return text.split("").reverse().join("");
+    if(!text || typeof text !== 'string') {
+      return '';
+    }
+
+    return text.split('').reverse().join('');
   };
 });
 
@@ -11,5 +15,15 @@ describe('reverse', function() {
   it('should reverse a string', inject(function(reverseFilter) {
     expect(reverseFilter('hello')).toEqual('olleh');
     expect(reverseFilter('aaa')).toEqual('aaa');
+  }));
+
+  it('should return an empty string when I give it null', inject(function(reverseFilter) {
+    expect(reverseFilter(null)).toEqual('');
+    expect(reverseFilter(undefined)).toEqual('');
+    expect(reverseFilter(123)).toEqual('');
+    expect(reverseFilter(true)).toEqual('');
+    expect(reverseFilter({})).toEqual('');
+    expect(reverseFilter([])).toEqual('');
+    expect(reverseFilter(function(){})).toEqual('');
   }));
 });
