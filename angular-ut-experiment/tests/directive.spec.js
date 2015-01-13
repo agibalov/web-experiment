4d44294189a1adb30203e91d2337cce4586623b0
loki2302 <loki2302@loki2302.me>
2015-01-13 23:41:25 +0300

    Added isolate scope directive test

--- angular-ut-experiment/tests/directive.spec.js
+++ angular-ut-experiment/tests/directive.spec.js
@@ -1,5 +1,5 @@
-describe('dummy directive', function() {
-  it('should work', function() {
+describe('directives', function() {
+  it('can have a directive that statically adds a class to an element', function() {
     var $injector = angular.injector(['ng', function($compileProvider) {
       $compileProvider.directive('dummy', function() {
         return function(scope, element) {
@@ -7,10 +7,65 @@ describe('dummy directive', function() {
         };
       });
     }]);
-    
+
     var $rootScope = $injector.get('$rootScope');
     var $compile = $injector.get('$compile');
     var element = $compile('<div dummy></div>')($rootScope);
     expect(element.hasClass('omg')).toBe(true);
+  });
+
+  describe('a directive that just renders a template', function() {
+    it('should work', function() {
+      var $injector = angular.injector(['ng', function($compileProvider) {
+        $compileProvider.directive('person', function() {
+          return {
+            template: 'name: {{name}}, age: {{age}}'
+          };
+        });
+      }]);
+
+      var $rootScope = $injector.get('$rootScope');
+      var $compile = $injector.get('$compile');
+      var element = $compile('<person/>')($rootScope);
+
+      $rootScope.$digest();
+      expect(element.html()).toBe('name: , age: ');
+
+      $rootScope.name = 'loki2302';
+      $rootScope.age = 40;
+      $rootScope.$digest();
+      expect(element.html()).toBe('name: loki2302, age: 40');
+    });
+  });
+
+  describe('a directive with an isolate scope', function() {
+    it('should work', function() {
+      var $injector = angular.injector(['ng', function($compileProvider) {
+        $compileProvider.directive('person', function() {
+          return {
+            restrict: 'E',
+            scope: {
+              personInfo: '=info'
+            },
+            template: 'name: {{personInfo.name}}, age: {{personInfo.age}}'
+          };
+        });
+      }]);
+
+      var $rootScope = $injector.get('$rootScope');
+      var $compile = $injector.get('$compile');
+      var element = $compile(
+        '<div>' + 
+        '<person id="p1" info="loki2302"/>' + 
+        '<person id="p2" info="andrey">' + 
+        '<div>'
+      )($rootScope);
+
+      $rootScope.loki2302 = { name: 'loki2302', age: 40 };
+      $rootScope.andrey = { name: 'Andrey', age: 30 };
+      $rootScope.$digest();
+      expect(element.find('#p1').text()).toBe('name: loki2302, age: 40');
+      expect(element.find('#p2').text()).toBe('name: Andrey, age: 30');
+    });
   });
 });
