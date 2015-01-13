350de2817c95304f715f4d03773f3b23e7c9188c
loki2302 <loki2302@loki2302.me>
2015-01-13 10:36:44 +0300

    Cleaned up the filter.spec - there's now explicit DI

--- angular-ut-experiment/tests/filter.spec.js
+++ angular-ut-experiment/tests/filter.spec.js
@@ -1,4 +1,4 @@
-angular.module('filterApp', [])
+angular.module('test.filter', [])
 .filter('reverse', function() {
   return function(text) {
     if(!text || typeof text !== 'string') {
@@ -10,20 +10,25 @@ angular.module('filterApp', [])
 });
 
 describe('reverse', function() {
-  beforeEach(module('filterApp'));
+  var reverseFilter;
+  beforeEach(function() {
+    var $injector = angular.injector(['ng', 'test.filter']);
+    var $filter = $injector.get('$filter');
+    reverseFilter = $filter('reverse');
+  });
 
-  it('should reverse a string', inject(function(reverseFilter) {
+  it('should reverse a string', function() {
     expect(reverseFilter('hello')).toEqual('olleh');
     expect(reverseFilter('aaa')).toEqual('aaa');
-  }));
+  });
 
-  it('should return an empty string when I give it null', inject(function(reverseFilter) {
+  it('should return an empty string when I give it something other than string', function() {
     expect(reverseFilter(null)).toEqual('');
     expect(reverseFilter(undefined)).toEqual('');
     expect(reverseFilter(123)).toEqual('');
     expect(reverseFilter(true)).toEqual('');
     expect(reverseFilter({})).toEqual('');
     expect(reverseFilter([])).toEqual('');
-    expect(reverseFilter(function(){})).toEqual('');
-  }));
+    expect(reverseFilter(function() {})).toEqual('');
+  });
 });
