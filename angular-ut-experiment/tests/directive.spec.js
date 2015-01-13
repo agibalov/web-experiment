5ef77dcee93f73f05dd5f3915a0552355a63c809
loki2302 <loki2302@loki2302.me>
2015-01-13 23:06:14 +0300

    Removed a dummy module from directive.spec

--- angular-ut-experiment/tests/directive.spec.js
+++ angular-ut-experiment/tests/directive.spec.js
@@ -1,13 +1,13 @@
-angular.module('test.directive', [])
-.directive('dummy', function() {
-  return function(scope, element) {
-    element.addClass('omg');
-  };
-});
-
 describe('dummy directive', function() {
   it('should work', function() {
-    var $injector = angular.injector(['ng', 'test.directive']);
+    var $injector = angular.injector(['ng', function($compileProvider) {
+      $compileProvider.directive('dummy', function() {
+        return function(scope, element) {
+          element.addClass('omg');
+        };
+      });
+    }]);
+    
     var $rootScope = $injector.get('$rootScope');
     var $compile = $injector.get('$compile');
     var element = $compile('<div dummy></div>')($rootScope);
