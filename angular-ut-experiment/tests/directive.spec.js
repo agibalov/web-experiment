df5eafcf73a97f214d708fbc589da8d7d0a9bf9d
loki2302 <loki2302@loki2302.me>
2015-01-14 11:18:10 +0300

    Added a directive with attributes test

--- angular-ut-experiment/tests/directive.spec.js
+++ angular-ut-experiment/tests/directive.spec.js
@@ -57,12 +57,44 @@ describe('directives', function() {
       var element = $compile(
         '<div>' + 
         '<person id="p1" info="loki2302"/>' + 
-        '<person id="p2" info="andrey">' + 
+        '<person id="p2" info="andrey"/>' + 
         '<div>'
       )($rootScope);
 
       $rootScope.loki2302 = { name: 'loki2302', age: 40 };
       $rootScope.andrey = { name: 'Andrey', age: 30 };
+      $rootScope.$digest();
+      expect(element.find('#p1').text()).toBe('name: loki2302, age: 40');
+      expect(element.find('#p2').text()).toBe('name: Andrey, age: 30');
+    });
+  });
+
+  describe('a directive with attributes', function() {
+    it('should work', function() {
+      var $injector = angular.injector(['ng', function($compileProvider) {
+        $compileProvider.directive('person', function() {
+          return {
+            restrict: 'E',
+            scope: {
+              name: '@'
+            },
+            template: 'name: {{name}}, age: {{age}}',
+            link: function(scope, element, attrs) {
+              scope.age = attrs.age;
+            }
+          };
+        });
+      }]);
+
+      var $rootScope = $injector.get('$rootScope');
+      var $compile = $injector.get('$compile');
+      var element = $compile(
+        '<div>' + 
+        '<person id="p1" name="loki2302" age="40" />' + 
+        '<person id="p2" name="Andrey" age="30" />' + 
+        '<div>'
+      )($rootScope);
+
       $rootScope.$digest();
       expect(element.find('#p1').text()).toBe('name: loki2302, age: 40');
       expect(element.find('#p2').text()).toBe('name: Andrey, age: 30');
