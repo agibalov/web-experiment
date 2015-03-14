225be80c73bd8cfb263087633294b59e51ea9ed8
loki2302 <loki2302@loki2302.me>
2015-03-14 16:15:57 +0300

    Added ResponseHandler test

--- angular-ut-experiment/tests/http.spec.js
+++ angular-ut-experiment/tests/http.spec.js
@@ -52,4 +52,113 @@ describe('$http', function() {
 
     expect(onSuccess).not.toHaveBeenCalled();
   }));
+
+  describe('Response handler', function() {
+    var responseHandler;
+
+    beforeEach(inject(function($q) {
+      responseHandler = new ResponseHandler()
+        .when(200, returnData())
+        .when(400, throwValidationError($q))
+        .otherwise(throwError($q));
+    }));
+
+    it('should successfully resolve to data when 200', inject(function($httpBackend, $http) {
+      $httpBackend.expect('GET', '/something').respond(200, { here: 'it is' });
+     
+      var onSuccess = jasmine.createSpy('onSuccess');
+      $http.get('/something')
+        .then(responseHandler.handle, responseHandler.handle)
+        .then(onSuccess);
+
+      $httpBackend.verifyNoOutstandingExpectation();
+
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingRequest();
+
+      expect(onSuccess).toHaveBeenCalledWith(jasmine.objectContaining({
+        here: 'it is'
+      }));
+    }));
+
+    it('should reject with validation error when 400', inject(function($httpBackend, $http) {
+      $httpBackend.expect('GET', '/something').respond(400, { here: 'it is' });
+     
+      var onError = jasmine.createSpy('onError');
+      $http.get('/something')
+        .then(responseHandler.handle, responseHandler.handle)
+        .then(null, onError);
+
+      $httpBackend.verifyNoOutstandingExpectation();
+
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingRequest();
+
+      expect(onError).toHaveBeenCalledWith(jasmine.objectContaining({
+        message: 'validation'
+      }));
+    }));
+
+    it('should reject with error when not 200 and not 400', inject(function($httpBackend, $http) {
+      $httpBackend.expect('GET', '/something').respond(500, { here: 'it is' });
+     
+      var onError = jasmine.createSpy('onError');
+      $http.get('/something')
+        .then(responseHandler.handle, responseHandler.handle)
+        .then(null, onError);
+
+      $httpBackend.verifyNoOutstandingExpectation();
+
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingRequest();
+
+      expect(onError).toHaveBeenCalledWith(jasmine.objectContaining({
+        message: '500'
+      }));
+    }));
+
+    function returnData() {
+      return function(httpResponse) {
+        return httpResponse.data;
+      };
+    };
+
+    function throwValidationError($q) {
+      return function(httpResponse) {
+        return $q.reject(new Error('validation'));
+      }; 
+    };
+
+    function throwError($q) {
+      return function(httpResponse) {
+        return $q.reject(new Error(httpResponse.status));
+      };
+    };
+
+    function ResponseHandler() {
+      var self = this;
+      self.handlers = {};
+      self.otherwiseHandlerFunc = null;
+
+      self.when = function(statusCode, handlerFunc) {
+        self.handlers[statusCode] = handlerFunc;
+        return self;
+      };
+
+      self.otherwise = function(handlerFunc) {
+        self.otherwiseHandlerFunc = handlerFunc;
+        return self;
+      };
+
+      self.handle = function(httpResponse) {
+        var statusCode = httpResponse.status;        
+        var handlerFunc = self.handlers[statusCode];
+        if(!handlerFunc) {
+          handlerFunc = self.otherwiseHandlerFunc;
+        }
+
+        return handlerFunc(httpResponse);
+      };
+    };
+  });
 });
