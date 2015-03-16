702d8ac2cdd6f6ed2db7206226617a10dd6b89e5
loki2302 <loki2302@loki2302.me>
2015-03-16 22:51:42 +0300

    Added more -related tests

--- angular-ut-experiment/tests/router.spec.js
+++ angular-ut-experiment/tests/router.spec.js
@@ -1,7 +1,8 @@
-// TODO: check if loads templates via $http
-// TODO: check the case when there's no such route and no .otherwise()
-// TODO: check how resolve works
+// TODO: check if it loads templates via $http
+// TODO: check how resolve works (and triggers $routeChangeError if there's a rejection)
+// TODO: check if $routeChangeError gets propagated to $exceptionHandler
 // TODO: check <ng-view/>
+// TODO: check how .otherwise() works
 
 describe('$routeProvider', function() {
   var $location;
@@ -40,23 +41,42 @@ describe('$routeProvider', function() {
     expect($route.current).toBeUndefined();
   });
 
-  it('should broadcast $routeChangeStart and $routeChangeSuccess when $location is changed', function() {
-    $location.path('/');
-    expect(onRouteChangeStart).not.toHaveBeenCalled();
-    expect(onRouteChangeSuccess).not.toHaveBeenCalled();
-    expect(onRouteChangeError).not.toHaveBeenCalled();
+  describe('when $location is changed', function() {
+    describe('and the new $location is recognized', function() {
+      beforeEach(function() {
+        $rootScope.$apply(function() {
+          $location.path('/');
+        });
+      });
 
-    $rootScope.$digest();
-    expect(onRouteChangeStart).toHaveBeenCalled();
-    expect(onRouteChangeSuccess).toHaveBeenCalled();
-    expect(onRouteChangeError).not.toHaveBeenCalled();
-  });
+      it('should broadcast $routeChangeStart and $routeChangeSuccess', function() {
+        expect(onRouteChangeStart).toHaveBeenCalled();
+        expect(onRouteChangeSuccess).toHaveBeenCalled();
+        expect(onRouteChangeError).not.toHaveBeenCalled();
+      });
 
-  it('should change the $route.current when $location is changed', function() {
-    $location.path('/');
-    $rootScope.$digest();
+      it('should update $route.current respectively', function() {
+        expect($route.current.controller).toBe('DummyController');
+        expect($route.current.template).toBe('template for dummy controller');
+      });
+    });
 
-    expect($route.current.controller).toBe('DummyController');
-    expect($route.current.template).toBe('template for dummy controller');
-  });
+    describe('and the new $location is not recognized', function() {
+      beforeEach(function() {
+        $rootScope.$apply(function() {
+          $location.path('/unknown-location');
+        });
+      });
+
+      it('should not broadcast any events', function() {
+        expect(onRouteChangeStart).not.toHaveBeenCalled();
+        expect(onRouteChangeSuccess).not.toHaveBeenCalled();
+        expect(onRouteChangeError).not.toHaveBeenCalled();
+      });
+
+      it('should keep $route.current undefined', function() {
+        expect($route.current).toBeUndefined();
+      });
+    });
+  });  
 });
