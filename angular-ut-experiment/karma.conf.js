90ebd90fb96f83fef4b4ffc5428528dcb93f0723
loki2302 <loki2302@loki2302.me>
2015-04-11 10:04:24 +0300

    Cleaning up the directory structure - extracted directive-related tests to /directives

--- angular-ut-experiment/karma.conf.js
+++ angular-ut-experiment/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-route/angular-route.js',
-      'tests/*.spec.js'
+      'tests/**/*.spec.js'
     ],
 
     autoWatch: true,
