e7ce529b57135b4cbbc8afc1bc4258d954a1d702
loki2302 <loki2302@loki2302.me>
2015-04-12 20:01:15 +0300

    Implemented reusable list editor directive

--- crud-experiment/src/app.js
+++ crud-experiment/src/app.js
@@ -1,69 +1,39 @@
 angular.module('app', [])
-.directive('entityEditor', function() {
+.directive('listEditor', function($parse) {
   return {
-    restrict: 'E',
-    scope: {
-      entity: '=',
-      onSubmitEntity: '&'
-    },
-    template: function(element) {
-      var html = element.html();
-      html =
-      '<form ng-submit="onSubmit($event)">' +
-      html +
-      '</form>';
-      return html;
-    },
-    link: function(scope, element, attrs, parentCtrl) {
-      console.log(scope.entity);
+    scope: true,
+    link: function(scope, element, attrs, controller, transclude) {
+      scope[attrs.as] = {
+        removeItem: function(item) {
+          var itemIndex = $parse(attrs.for)(scope).indexOf(item);
+          if(itemIndex < 0) {
+            throw new Error('Did not find an item');
+          }
 
-      scope.onSubmit = function(e) {
-        e.preventDefault();
-        scope.onSubmitEntity({ entity: scope.entity });
-      };
-    }
-  };
-})
-.directive('relatedEntityEditor', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      entities: '='
-    },
-    template: function(element) {
-      return element.html();
-    },
-    link: function(scope) {
-      scope.addItem = function(item) {
-        console.log('addItemNew', item);
-        scope.entities.push(item);
-      };
-
-      scope.removeItem = function(item) {
-        console.log('removeItemNew', item);
-
-        var itemIndex = scope.entities.indexOf(item);
-        if(itemIndex < 0) {
-          throw new Error('Did not find the item');
+          $parse(attrs.for)(scope).splice(itemIndex, 1);
+        },
+        newItem: {},
+        addItem: function(item) {
+          $parse(attrs.for)(scope).push(scope[attrs.as].newItem);
+          scope[attrs.as].newItem = {};
         }
-
-        scope.entities.splice(itemIndex, 1);
       };
     }
   };
 })
 .controller('AppController', function($scope) {
   $scope.messageFromController = 'message from controller';
-  $scope.myCategory = {
-    name: 'entity name here',
-    posts: [
-      { id: 1, title: 'porn' },
-      { id: 2, title: 'lesbians' },
-      { id: 3, title: 'more porn' }
-    ]
-  };
 
-  $scope.handleEntity = function(c) {
-    console.log('Got updated category', c);
+  $scope.course = {
+    title: 'Math',
+    teachers: [
+      { name: 'Teacher One' },
+      { name: 'Teacher Two' }
+    ],
+    students: [
+      { name: 'Student One' },
+      { name: 'Student Two' },
+      { name: 'Student Three' }
+    ]
   };
 });
