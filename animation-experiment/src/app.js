6969f4addd9b225d183a2d4f43274e9b428b252f
loki2302 <loki2302@loki2302.me>
2015-04-16 22:43:45 +0300

    Added ngRepeat animation demo

--- animation-experiment/src/app.js
+++ animation-experiment/src/app.js
@@ -1,4 +1,21 @@
 angular.module('app', ['ngAnimate'])
 .controller('AppController', function($scope) {
   $scope.message = 'hello world';
+  $scope.items = [
+    'hello',
+    'there'
+  ];
+
+  $scope.addItem = function() {
+    $scope.items.push(Math.random());
+  };
+
+  $scope.removeItem = function(item) {
+    var itemIndex = $scope.items.indexOf(item);
+    if(itemIndex === -1) {
+      return;
+    }
+
+    $scope.items.splice(itemIndex, 1);
+  };
 });
