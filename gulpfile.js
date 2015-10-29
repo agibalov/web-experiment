135fea15d733ac3bc23e3b819fe4904b53ad4b27
loki2302 <loki2302@loki2302.me>
2015-10-29 23:14:44 +0300

    Initial version

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var del = require('del');
+
+gulp.task('clean', function() {
+  return del(['dist/']);
+});
+
+gulp.task('copy', function() {
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('webpack', function() {
+  return gulp.src('./src/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'everything.js'
+      },
+      module: {
+        loaders: [
+          { test: /\.css$/, loader: 'style!css' },
+          { test: /\.txt$/, loader: 'raw' }
+        ]
+      }
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['clean', 'copy', 'webpack']);
---
+++ src/entry.js
@@ -0,0 +1,4 @@
+require('./style.css');
+var add = require('./add');
+var message = require('./2and3is.txt');
+console.log(message.trim(), add(2, 3));
