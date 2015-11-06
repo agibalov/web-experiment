09cf6439981be896d25fa7ab73bc4f0a6f4bd378
loki2302 <loki2302@loki2302.me>
2015-11-06 09:44:16 +0300

    Added a dummy class inheritance test

--- test/classes.js
+++ test/classes.js
@@ -1,14 +1,40 @@
 import { expect } from 'chai'
 
-describe('class', () => {
-  it('should work', () => {
-    class Calculator {
-      add(a, b) {
-        return a + b
+describe('classes', () => {
+  describe('the most primitive case', () => {
+    it('should work', () => {
+      class Calculator {
+        add(a, b) {
+          return a + b
+        }
       }
-    }
 
-    const calculator = new Calculator()
-    expect(calculator.add(2, 3)).to.equal(5)
+      const calculator = new Calculator()
+      expect(calculator.add(2, 3)).to.equal(5)
+    })
+  })
+
+  describe('inheritance', () => {
+    it('should work', () => {
+      class Animal {
+        makeSound() {
+          return 'default sound'
+        }
+      }
+
+      class Cat extends Animal {
+        makeSound() {
+          return 'meow'
+        }
+
+        makeParentSound() {
+          return super.makeSound()
+        }
+      }
+
+      const cat = new Cat()
+      expect(cat.makeSound()).to.equal('meow')
+      expect(cat.makeParentSound()).to.equal('default sound')
+    })
   })
 })
