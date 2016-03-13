6d7efeaae3c671819cd92b5a08de56422c100623
loki2302 <loki2302@loki2302.me>
2016-03-13 16:06:25 -0400

    Added a common layout to router-app

--- router-app/index.js
+++ router-app/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Link, browserHistory, hashHistory } from 'react-router'
 
-const Page1 = () => {
+const App = ({children}) => {
+  return <div>
+    <h1>App</h1>
+    <ul>
+      <li><Link to={`/`}>Page 1</Link></li>
+      <li><Link to={`/page2`}>Page 2</Link></li>
+      <li><Link to={`/addNumbers/7/8`}>Add Numbers</Link></li>
+    </ul>
+    {children}
+    <hr />
+    &copy; 2016
+  </div>
+}
+
+const Page1 = (props, { router }) => {
   const a = 2
   const b = 3
   return <div>
     I am page 1. Go to <Link to={`/page2`}>Page 2</Link>. Go to <Link to={`/addNumbers/${a}/${b}`}>Add numbers</Link>.
+    <button type="button" onClick={() => {
+      router.push('/page2')
+    }}>Go to Page 2</button>
   </div>
+}
+Page1.contextTypes = {
+  router: React.PropTypes.object.isRequired
 }
 
 const Page2 = () => {
@@ -20,16 +40,18 @@ const AddNumbers = ({ params }) => {
   const a = parseInt(params.a)
   const b = parseInt(params.b)
   const sum = a + b
-  return <div>Add numbers {a} and {b}: {sum}. Go to <Link to="/">Page 1</Link></div>
+  return <div>{a} and {b} is {sum}. Go to <Link to="/">Page 1</Link></div>
 }
 
 let history = hashHistory // or browserHistory (HTML5 URLs)
 
 render(
   <Router history={history}>
-    <Route path="/" component={Page1} />
-    <Route path="/page2" component={Page2} />
-    <Route path="/addNumbers/:a/:b" component={AddNumbers} />
+    <Route path="/" component={App}>
+      <IndexRoute component={Page1} />
+      <Route path="page2" component={Page2} />
+      <Route path="addNumbers/:a/:b" component={AddNumbers} />
+    </Route>
   </Router>,
   document.getElementById('root')
 )
