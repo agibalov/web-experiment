f332b8ba491ecca07c05590032584b7f1e30c4d8
loki2302 <loki2302@loki2302.me>
2016-03-14 23:35:03 -0400

    Added a dummy 'create todo' page

--- todo-app2/index.js
+++ todo-app2/index.js
@@ -3,6 +3,13 @@ import { render } from 'react-dom'
 import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 import { connect } from 'react-redux'
 
+const createTodo = (text) => {
+  return {
+    type: 'CREATE_TODO',
+    text: text
+  }
+}
+
 const App = ({children}) => {
   return <div>
     <h2>Todo App</h2>
@@ -21,9 +28,32 @@ const TodoListPage = connect(state => {
 })(({todos}) => {
   return <div>
     <h3>Todo List Page</h3>
+    <Link to="/new">Create Todo</Link>
     <ul>
       { todos.map(todo => <li key={todo.id}>{todo.text} (<Link to={`/${todo.id}`}>see</Link>)</li>) }
     </ul>
+  </div>
+})
+
+const CreateTodoPage = connect(state => {
+  return {}
+}, dispatch => {
+  return {
+    createTodo: (text) => {
+      console.log('create todo', text)
+      dispatch(createTodo(text))
+    }
+  }
+})(({createTodo}, {router}) => {
+  let textElement
+  return <div>
+    <h3>Create Todo Page</h3>
+    <input type="text" ref={e => textElement = e} />
+    <button type="button" onClick={() => {
+      createTodo(textElement.value)
+      // how do I get the newly created todo id?
+      // who should send the user to the new todo page?
+    }}>Create</button>
   </div>
 })
 
@@ -45,8 +75,23 @@ const TodoItemPage = connect((state, {params}) => {
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
+let lastId = 3
 const store = createStore((state, action) => {
-  return state
+  switch(action.type) {
+    case 'CREATE_TODO':
+      const todo = {
+        id: lastId++,
+        text: action.text
+      }
+      return {
+        todos: [
+          ...state.todos,
+          todo
+        ]
+      }
+    default:
+      return state
+  }
 }, {
   todos: [{id:1, text:'do something'}, {id:2, text:'do something else'}]
 })
@@ -56,70 +101,9 @@ render(
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={TodoListPage} />
+        <Route path="/new" component={CreateTodoPage} />
         <Route path="/:id" component={TodoItemPage} />
       </Route>
     </Router>
   </Provider>,
   document.getElementById('root'))
-
-/*import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, IndexRoute, Link, browserHistory, hashHistory } from 'react-router'
-
-const App = ({children}) => {
-  return <div>
-    <h1>App</h1>
-    <ul>
-      <li><Link to={`/`}>Page 1</Link></li>
-      <li><Link to={`/page2`}>Page 2</Link></li>
-      <li><Link to={`/addNumbers/7/8`}>Add Numbers</Link></li>
-    </ul>
-    {children}
-    <hr />
-    &copy; 2016
-  </div>
-}
-
-const Page1 = (props, { router }) => {
-  const a = 2
-  const b = 3
-  return <div>
-    I am page 1. Go to <Link to={`/page2`}>Page 2</Link>. Go to <Link to={`/addNumbers/${a}/${b}`}>Add numbers</Link>.
-    <button type="button" onClick={() => {
-      router.push('/page2')
-    }}>Go to Page 2</button>
-  </div>
-}
-Page1.contextTypes = {
-  router: React.PropTypes.object.isRequired
-}
-
-const Page2 = () => {
-  return <div>
-    I am page 2. Go to <Link to={`/`}>Page 1</Link>
-  </div>
-}
-
-const AddNumbers = ({ params }) => {
-  const a = parseInt(params.a)
-  const b = parseInt(params.b)
-  const sum = a + b
-  return <div>{a} and {b} is {sum}. Go to <Link to="/">Page 1</Link></div>
-}
-
-let history = hashHistory // or browserHistory (HTML5 URLs)
-
-render(
-  <Router history={history}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Page1} />
-      <Route path="page2" component={Page2}
-        onEnter={() => { console.log('Page 2 - onEnter') }}
-        onLeave={() => { console.log('Page 2 - onLeave') }}
-      />
-      <Route path="addNumbers/:a/:b" component={AddNumbers} />
-    </Route>
-  </Router>,
-  document.getElementById('root')
-)
-*/
