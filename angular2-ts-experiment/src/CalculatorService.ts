232db178541bc01d892caf8e81a5d36d57ccfb16
loki2302 <loki2302@loki2302.me>
2016-09-25 12:56:08 -0400

    Removing angular2-ts-experiment

--- angular2-ts-experiment/src/CalculatorService.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from 'angular2/core'
-
-@Injectable()
-export class CalculatorService {
-    addNumbers(a: number, b: number): number {
-        console.log('CalculatorService!')
-        return a + b
-    }
-}
