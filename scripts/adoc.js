1f72929ef96d021f5eb0b93742556270ab9976eb
loki2302 <loki2302@loki2302.me>
2016-11-06 16:26:51 -0500

    Added a dummy Asciidoctor renderer

---
+++ scripts/adoc.js
@@ -0,0 +1,23 @@
+var process = require('process');
+var asciidoctor = require('asciidoctor.js')();
+
+var opal = asciidoctor.Opal;
+var opts = opal.hash2(['base_dir', 'safe'], {
+    'base_dir': process.cwd(),
+    'safe': 'safe'
+});
+
+var processor = asciidoctor.Asciidoctor(true);
+
+function render(data, locals) {
+    var html = processor.$convert(data.text, opts);
+    html += `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/darcula.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/highlight.min.js"></script>
+<script>hljs.initHighlighting()</script>
+    `;
+
+    return html;
+}
+
+hexo.extend.renderer.register('adoc', 'html', render, true);
