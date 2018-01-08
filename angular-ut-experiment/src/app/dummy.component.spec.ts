73d490e25c754d796217a0902cb605b6cd5507d1
agibalov <loki2302@loki2302.me>
2018-01-08 05:09:24 -0500

    Added Component lifecycle test

--- angular-ut-experiment/src/app/dummy.component.spec.ts
+++ angular-ut-experiment/src/app/dummy.component.spec.ts
@@ -1,15 +1,15 @@
 import {TestBed, async} from '@angular/core/testing';
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'dummy',
-  template: `<div>hello {{who}}</div>`
-})
-export class DummyComponent {
-  who = 'world';
-}
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 describe('DummyComponent', () => {
+  @Component({
+    selector: 'dummy',
+    template: `<div>hello {{who}}</div>`
+  })
+  class DummyComponent {
+    who = 'world';
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -33,5 +33,58 @@ describe('DummyComponent', () => {
 
     fixture.detectChanges();
     expect(compiled.querySelector('div').textContent).toBe('hello you');
+  }));
+});
+
+describe('Lifecycle', () => {
+  @Component({
+    selector: 'dummy',
+    template: `hello`
+  })
+  class DummyComponent implements OnInit, OnDestroy {
+    public log: string[];
+
+    ngOnInit(): void {
+      if(this.log != null) {
+        this.log.push('onInit()');
+      }
+    }
+
+    ngOnDestroy(): void {
+      if(this.log != null) {
+        this.log.push('onDestroy()');
+      }
+    }
+  }
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DummyComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should work', async(() => {
+    const fixture = TestBed.createComponent(DummyComponent);
+    const dummyComponent = fixture.debugElement.componentInstance;
+    const log: string[] = [];
+    dummyComponent.log = log;
+
+    log.push('before onInit()');
+    fixture.detectChanges();
+    log.push('after onInit()');
+    log.push('before onDestroy()')
+    fixture.destroy();
+    log.push('after onDestroy()');
+
+    expect(log).toEqual([
+      'before onInit()',
+      'onInit()',
+      'after onInit()',
+      'before onDestroy()',
+      'onDestroy()',
+      'after onDestroy()'
+    ]);
   }));
 });
