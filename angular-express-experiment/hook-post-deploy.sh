1de692c7ed18896362231d6e86d6de839f53451e
agibalov <loki2302@loki2302.me>
2018-02-03 22:44:27 -0500

    Experimenting with PM2-based deployment #12

--- angular-express-experiment/hook-post-deploy.sh
+++ angular-express-experiment/hook-post-deploy.sh
@@ -2,6 +2,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
 
+nvm use --lts
 npm install
 npm run app:build
 pm2 startOrRestart ecosystem.prod.yaml
