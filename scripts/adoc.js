98acdf2d427533a849ac95c0389a96ac86d51c3f
agibalov <loki2302@loki2302.me>
2018-07-01 15:15:33 -0400

    Cleaning up

--- scripts/adoc.js
+++ scripts/adoc.js
@@ -1,14 +1,4 @@
-//var process = require('process');
 var asciidoctor = require('asciidoctor.js')();
-
-/*
-var opal = asciidoctor.Opal;
-var opts = opal.hash2(['base_dir', 'safe'], {
-    'base_dir': process.cwd(),
-    'safe': 'safe'
-});
-
-var processor = asciidoctor.Asciidoctor(true);*/
 
 function render(data, locals) {
     var html = asciidoctor.convert(data.text);
