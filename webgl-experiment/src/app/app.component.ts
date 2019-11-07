dbd7e0a51aa80d566802c0a9997a2a93fa4a704c
agibalov <andrey.agibalov@gmail.com>
2019-11-06 19:25:04 -0500

    LABS-1158: add animation

--- webgl-experiment/src/app/app.component.ts
+++ webgl-experiment/src/app/app.component.ts
@@ -8,13 +8,24 @@ import { mat4 } from 'gl-matrix';
 })
 export class AppComponent implements AfterViewInit {
     @ViewChild('canvas1', {static: false}) canvasRef: ElementRef;
+    width: number;
+    height: number;
+    triangleVertexPositionBuffer: WebGLBuffer;
+    readonly triangleVertexPositionBufferItemSize = 3;
+    readonly triangleVertexPositionBufferNumItems = 6;
+    positionLocation: number;
+    perspectiveLocation: WebGLUniformLocation;
+    modelViewLocation: WebGLUniformLocation;
 
     ngAfterViewInit(): void {
         const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
-        const context = canvas.getContext('webgl') as WebGLRenderingContext;
+        this.width = canvas.width;
+        this.height = canvas.height;
 
-        const vertexShader = context.createShader(context.VERTEX_SHADER);
-        context.shaderSource(vertexShader, `
+        const gl = canvas.getContext('webgl') as WebGLRenderingContext;
+
+        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertexShader, `
             attribute vec3 Position;
             
             uniform mat4 u_ModelViewMatrix;
@@ -24,68 +35,83 @@ export class AppComponent implements AfterViewInit {
                 gl_Position = u_PerspectiveMatrix * u_ModelViewMatrix * vec4(Position, 1.0);
             }
         `);
-        context.compileShader(vertexShader);
-        if (!context.getShaderParameter(vertexShader, context.COMPILE_STATUS)) {
+        gl.compileShader(vertexShader);
+        if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
             throw new Error('Failed to compile vertex shader');
         }
 
-        const fragmentShader = context.createShader(context.FRAGMENT_SHADER);
-        context.shaderSource(fragmentShader, `
+        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragmentShader, `
             precision mediump float;
             
             void main(void) {
-                gl_FragColor = vec4(0.9, 0.3, 0.6, 1.0);
+                gl_FragColor = vec4(1.0, 0.3, 0.0, 1.0);
             }
         `);
-        context.compileShader(fragmentShader);
-        if (!context.getShaderParameter(fragmentShader, context.COMPILE_STATUS)) {
+        gl.compileShader(fragmentShader);
+        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
             throw new Error('Failed to compile fragment shader');
         }
 
-        const program = context.createProgram();
-        context.attachShader(program, fragmentShader);
-        context.attachShader(program, vertexShader);
-        context.linkProgram(program);
-        if (!context.getProgramParameter(program, context.LINK_STATUS)) {
+        const program = gl.createProgram();
+        gl.attachShader(program, fragmentShader);
+        gl.attachShader(program, vertexShader);
+        gl.linkProgram(program);
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
             throw new Error('Failed to link program');
         }
 
-        context.useProgram(program);
+        gl.useProgram(program);
 
-        const positionLocation = context.getAttribLocation(program, 'Position');
-        context.enableVertexAttribArray(positionLocation);
+        this.positionLocation = gl.getAttribLocation(program, 'Position');
+        gl.enableVertexAttribArray(this.positionLocation);
 
-        const perspectiveLocation = context.getUniformLocation(program, 'u_PerspectiveMatrix');
-        const modelViewLocation = context.getUniformLocation(program, 'u_ModelViewMatrix');
+        this.perspectiveLocation = gl.getUniformLocation(program, 'u_PerspectiveMatrix');
+        this.modelViewLocation = gl.getUniformLocation(program, 'u_ModelViewMatrix');
 
-        const triangleVertexPositionBuffer = context.createBuffer();
-        context.bindBuffer(context.ARRAY_BUFFER, triangleVertexPositionBuffer);
-        context.bufferData(context.ARRAY_BUFFER, new Float32Array([
-            0, 1, 0,
+        this.triangleVertexPositionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
             -1, -1, 0,
-            1, -1, 0
-        ]), context.STATIC_DRAW);
-        const triangleVertexPositionBufferItemSize = 3;
-        const triangleVertexPositionBufferNumItems = 3;
+            -1, 1, 0,
+            1, 1, 0,
+            1, 1, 0,
+            1, -1, 0,
+            -1, -1, 0
+        ]), gl.STATIC_DRAW);
 
-        context.clearColor(0.5, 0.5, 0.5, 1);
-        context.enable(context.DEPTH_TEST);
+        requestAnimationFrame(timestamp => this.render(gl, timestamp));
+    }
 
-        context.viewport(0, 0, canvas.width, canvas.height);
-        context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
+    startTimestamp: number|null = null;
+
+    render(gl: WebGLRenderingContext, timestamp: number) {
+        if (this.startTimestamp === null) {
+            this.startTimestamp = timestamp;
+        }
+
+        const elapsedTime = timestamp - this.startTimestamp;
 
         const perspectiveMatrix = mat4.create();
-        mat4.perspective(perspectiveMatrix, 45, canvas.width / canvas.height, 0.1, 100.0);
+        mat4.perspective(perspectiveMatrix, 45, this.width / this.height, 0.1, 100.0);
 
         const modelViewMatrix = mat4.create();
         mat4.fromTranslation(modelViewMatrix, [0, 0, -4]);
+        mat4.rotate(modelViewMatrix, modelViewMatrix, elapsedTime * 0.001, [0, 0.7, 1]);
 
-        context.bindBuffer(context.ARRAY_BUFFER, triangleVertexPositionBuffer);
-        context.vertexAttribPointer(positionLocation, triangleVertexPositionBufferItemSize, context.FLOAT, false, 0, 0);
+        gl.clearColor(0.5, 0.5, 0.5, 1);
+        gl.enable(gl.DEPTH_TEST);
 
-        context.uniformMatrix4fv(perspectiveLocation, false, perspectiveMatrix);
-        context.uniformMatrix4fv(modelViewLocation, false, modelViewMatrix);
+        gl.viewport(0, 0, this.width, this.height);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        context.drawArrays(context.TRIANGLES, 0, triangleVertexPositionBufferNumItems);
+        gl.uniformMatrix4fv(this.perspectiveLocation, false, perspectiveMatrix);
+        gl.uniformMatrix4fv(this.modelViewLocation, false, modelViewMatrix);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
+        gl.vertexAttribPointer(this.positionLocation, this.triangleVertexPositionBufferItemSize, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(gl.TRIANGLES, 0, this.triangleVertexPositionBufferNumItems);
+
+        requestAnimationFrame(timestamp => this.render(gl, timestamp));
     }
 }
