fd2a4e4af6c0d474db867c7cca1decc2de0e28c8
Andrey Agibalov <andrey.agibalov@gmail.com>
2025-09-19 02:05:15 -0400

    Save progress

--- chrome-extension-experiment/README.md
+++ chrome-extension-experiment/README.md
@@ -4,4 +4,14 @@ Figuring out how to build Chrome extensions.
 
 ## How to do things
 
-* Run `npm run preview` in one terminal tab and `npx web-ext run --target=chromium --chromium-binary="$(which google-chrome)" --source-dir=dist` in another for development.
+### Development
+
+In 3 separate terminal tabs:
+
+* `npm run preview` - continuously builds to `/dist`
+* `npx web-ext run --target=chromium --chromium-binary="$(which google-chrome)" --source-dir=dist` - deploys `/dist` to Chrome
+* `npm run dev` - serves dummy API
+
+### Making production builds
+
+TODO
--- chrome-extension-experiment/vite.config.ts
+++ chrome-extension-experiment/vite.config.ts
@@ -2,7 +2,25 @@ import { defineConfig } from "vite"
 import { crx } from "@crxjs/vite-plugin"
 import manifest from "./manifest.json"
 
+const dummyApi = () => ({
+    name: 'dummy-api',
+    configureServer(server) {
+        server.middlewares.use("/hello", (req, res, next) => {
+            if (req.method === "GET" || req.method === 'POST') {
+                console.log(`${req.method} /hello (body=${req.body})`)
+                res.setHeader("Content-Type", "application/json")
+                res.end(JSON.stringify({ message: `Hello World! ${new Date().toISOString()}` }))
+            } else {
+                next()
+            }
+        })
+    },
+})
+
 export default defineConfig({
-  plugins: [crx({ manifest })],
-  build: { emptyOutDir: true }
+    plugins: [crx({ manifest }), dummyApi()],
+    build: { emptyOutDir: true },
+    server: {
+        port: 8080
+    }
 })
