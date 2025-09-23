5565b17c7fb43cd9d4d084e2496097329d6dec50
Andrey Agibalov <andrey.agibalov@gmail.com>
2025-09-23 15:48:05 -0400

    Adjust CORS origins

--- react-admin-experiment/todo_app_backend/app.py
+++ react-admin-experiment/todo_app_backend/app.py
@@ -195,7 +195,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
